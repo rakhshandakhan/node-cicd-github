4edfe82a98e8e4d31e6288172e0622d8b7afc884
Rakhshanda Jabin Khan <rakhshandajabin.khan@gmail.com>
2024-02-27 15:01:22 +0530

    message updated

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -4,7 +4,7 @@ app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        context.log(`Http function processed request for url updates "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
 
